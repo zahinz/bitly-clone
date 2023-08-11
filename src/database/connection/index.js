cbb4ce8cb6f5111b51f5efb71622b2bc9584e13e
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-08-11 10:49:32 +0800

    fix ssl last

--- src/database/connection/index.js
+++ src/database/connection/index.js
@@ -1,14 +1,22 @@
 import { Sequelize } from "sequelize";
 import config from "../../config";
 
-const { database, user, password, host, port, ssl } = config.postgres;
+const { database, user, password, host, port } = config.postgres;
 
 export const postgresConnection = new Sequelize(database, user, password, {
   host: host,
   port: port,
   dialect: "postgres",
   logging: config.nodeEnv === "development",
-  ssl: true,
+  dialectOptions: {
+    ssl:
+      config.nodeEnv === "production"
+        ? {
+            require: true,
+            rejectUnauthorized: false,
+          }
+        : false,
+  },
 });
 
 export default postgresConnection;
